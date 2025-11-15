fbc49a169028be10a7edf9b06b32bd29df216046
Mikhail Popov <popov_muy@t.top-academy.ru>
2025-11-15 18:08:45 +0300

    day 2

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 import uvicorn
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI(title="Моё первое веб приложение")
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def home():
-    return "Hello world"
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    context = {
+        "request": request,
+        "title": "Главная страница",
+        "main_text": "Здесь будет текст для главной страницы с описанием"
+    }
+    return templates.TemplateResponse("index.html", context=context)
 
-@app.get("/hello")
-def html_hello():
-    html = """
-    <html>
-        <head>
-        <title>Моё первое веб приложение</title>
-        </head>
-        <body>
-            <h1>Hello world</h1>
-            <p>Наша первая страница</p>
-            <a href="http://localhost:8000/file">На страницу file</a>    
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html)
 
-@app.get("/file")
-def from_file():
-    with open("templates/index.html", "r", encoding="utf-8") as fl:
-        content = fl.read()
-    return HTMLResponse(content=content)
+@app.get("/about", response_class=HTMLResponse)
+def about(request: Request):
+    context = {
+        "request": request,
+        "title": "О нас",
+        "people_count": 15,
+        "mission": "Наша цель - помогать людям переходить дорогу"
+    }
+    return templates.TemplateResponse("about.html", context=context)
+
+@app.get("/contacts", response_class=HTMLResponse)
+def contacts(request: Request):
+    context = {
+        "request": request,
+        "title": "Контакты",
+        "adress": "Павловский Посад, ул. Павловская д. 26",
+        "phone": "8 (800) 555-35-35",
+        "email": "top@secret.com"
+    }
+    return templates.TemplateResponse("contacts.html", context=context)
+    
 
 if __name__  == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
